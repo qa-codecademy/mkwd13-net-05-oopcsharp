f41a65ee217b926ef19ebbcfaa688354a50606be
trajanstevkovski <trajanstevkovski@gmail.com>
2025-03-05 21:05:15 +0100

    Class04 methods

--- G3/Class04/QA.SeeSharp.Class04/QA.SeeSharp.Class04.App/Program.cs
+++ G3/Class04/QA.SeeSharp.Class04/QA.SeeSharp.Class04.App/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Xml.Linq;
+﻿using System.Data.SqlTypes;
 
 namespace QA.SeeSharp.Class04.App
 {
@@ -6,6 +6,12 @@ namespace QA.SeeSharp.Class04.App
     {
         static void Main(string[] args)
         {
+            Exercise01();
+
+            Console.ReadLine();
+            //Console.Write("Please enter number: ");
+            //Console.ReadLine();
+
             SayHello("Trajan"); // fnc with no return
             string fullName = GetFullName("Trajan", "Stevkovski"); // fnc that return string
             Console.WriteLine(fullName);
@@ -17,7 +23,7 @@ namespace QA.SeeSharp.Class04.App
             Console.WriteLine(result);
 
             string[] names = GetNames();
-            foreach(string name in names)
+            foreach (string name in names)
             {
                 Console.WriteLine(name);
             }
@@ -60,13 +66,79 @@ namespace QA.SeeSharp.Class04.App
         public static string[] GetNames()
         {
             string[] names = new string[10];
-            for(int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
                 string input = Console.ReadLine();
                 names[i] = input;
             }
 
             return names;
+        }
+        // EXERCISE 01
+        public static void Exercise01()
+        {
+            string operation = GetOperator();
+            int number1 = GetNumber("Please enter the first number");
+            int number2 = GetNumber("Please enter the second number");
+            Calculator(operation, number1, number2);
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static string GetOperator()
+        {
+            bool isValidOperator = false;
+            string operation = string.Empty;
+            while (!isValidOperator)
+            {
+                Console.WriteLine("Please enter operation (+/-)");
+                operation = Console.ReadLine();
+                if (operation == "+" || operation == "-")
+                {
+                    isValidOperator = true;
+                }
+            }
+            return operation;
+        }
+
+        public static int GetNumber(string message)
+        {
+            Console.WriteLine(message);
+            int number = default; // 0
+            while (true)
+            {
+                string input = Console.ReadLine();
+                bool isValidInput = int.TryParse(input, out number);
+                if (isValidInput)
+                {
+                    break;
+                }
+            }
+            return number;
+        }
+
+        public static void Calculator(string operation, int a, int b)
+        {
+            switch(operation)
+            {
+                case "+":
+                    Console.WriteLine(Sum(a, b));
+                    break;
+                case "-":
+                    Console.WriteLine(Substract(b, a));
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
         }
     }
 }
