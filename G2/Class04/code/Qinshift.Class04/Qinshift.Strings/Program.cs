bea330f5f1bf631ed53a28a533b226c614e54537
Ilija Mitev <ilija_mitev_mk@yahoo.com>
2025-03-06 18:42:32 +0100

    String Concatenation example

---
+++ G2/Class04/code/Qinshift.Class04/Qinshift.Strings/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("\n================== STRINGS ==================\n");
+Console.ResetColor();
+
+
+#region String Concatenation
+
+Console.WriteLine("\n\n============== String Concatenation ==============\n");
+
+// Normal strings
+string firstName = "Bob";
+string lastName = "Bobsky";
+
+// String Concatenation
+
+// 1) using '+' operator
+string hello1 = "Hello " + firstName + " " + lastName;
+Console.WriteLine(hello1);
+// 2) *using String Interpolation*
+string hello2 = $"Hello {firstName} {lastName}";
+Console.WriteLine(hello2);
+// 3) using string.Format
+string hello3 = string.Format("Hello {0} {1}", lastName, firstName);
+Console.WriteLine(hello3);
+// 4) using String.Concat
+string hello4 = string.Concat("Hello ", firstName, " ", lastName);
+Console.WriteLine(hello4);
+// 5) using StringBuilder (only for info, will be explained in detail further on..)
+
+#endregion
+
+
+#region String Formatting
+
+Console.WriteLine("\n\n============== String Formatting ==============\n");
+
+
+
+#endregion
+
+Console.ReadLine();
