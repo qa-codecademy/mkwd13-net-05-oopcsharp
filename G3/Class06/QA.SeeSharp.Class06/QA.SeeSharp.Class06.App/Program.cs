c494ab0137ba9790a4e8ed338e612a446178e1a1
trajanstevkovski <trajanstevkovski@gmail.com>
2025-03-10 20:54:03 +0100

    Class05 Classes

---
+++ G3/Class06/QA.SeeSharp.Class06/QA.SeeSharp.Class06.App/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace QA.SeeSharp.Class06.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.FirstName = "Trajan";
+            user.LastName = "Stevkovski";
+            Console.WriteLine(user.FirstName);
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
