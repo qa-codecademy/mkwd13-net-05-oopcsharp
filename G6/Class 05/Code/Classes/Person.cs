54c44bd027d3571206f51b4214b157369700be36
tijanastojanovska <stojanovska_tijana@outlook.com>
2025-03-11 20:47:53 +0100

    Class 05

---
+++ G6/Class 05/Code/Classes/Person.cs
@@ -0,0 +1,44 @@
+﻿namespace Classes //the path from our sln to our class => in our case the project Classes
+{
+    public class Person //default is internal
+    {
+        //we can access FirstName and LastName and Age from anywhere
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        //we can access _ssn only from the Person class and its methods
+        private long _ssn {  get; set; }
+
+        //Constructor
+        //Constructor is public and has the same name as the class
+        //we need it in order to instantiate objects
+        //we can have more than one constructor
+        public Person() {
+            _ssn = new Random().Next(100000, 999999); //gets a random number from 100000 to 999999
+        }
+
+        public Person(string firstName, string lastName, int age, long ssn) {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            if(ssn >= 100000 && ssn<= 999999)
+            {
+                _ssn= ssn;
+            }
+            else
+            {
+                _ssn = new Random().Next(100000, 999999);
+            }
+        }
+
+        //we can create more constructors with different params
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = 0;
+            _ssn = new Random().Next(100000, 999999);
+        }
+    }
+}
---
+++ G6/Class 05/Code/DateTime/Program.cs
@@ -0,0 +1,61 @@
+﻿//DateTime
+
+DateTime emptyDateTime = new DateTime();
+Console.WriteLine(emptyDateTime);
+
+//create DateTime with current Date and Time(local date and time, from the server)
+DateTime currentDateTime = DateTime.Now;
+Console.WriteLine(currentDateTime);
+
+DateTime universalDateTime = DateTime.UtcNow;
+Console.WriteLine(universalDateTime);
+
+DateTime specificDate = new DateTime(1999, 07, 28); //year month day
+Console.WriteLine(specificDate);
+
+DateTime specificDateTime = new DateTime(2024, 07, 28, 12, 34, 45); //year, month, day, hour, minute, second
+Console.WriteLine(specificDateTime);
+
+int day = currentDateTime.Day;
+Console.WriteLine(day);
+
+int month = currentDateTime.Month;
+Console.WriteLine(month);
+
+int year = currentDateTime.Year;
+Console.WriteLine(year);
+
+var dayOfWeek = currentDateTime.DayOfWeek;
+Console.WriteLine(dayOfWeek);
+
+DateTime firstDateTime = new DateTime(2000, 2, 3, 15, 55, 20);
+DateTime secondDateTime = new DateTime(2025, 11, 3, 19, 28, 20);
+
+var difference = secondDateTime - firstDateTime;
+Console.WriteLine(difference);
+
+int days = difference.Days;
+int hours = difference.Hours;
+int minutes = difference.Minutes;
+Console.WriteLine(days);
+Console.WriteLine(minutes);
+Console.WriteLine(hours);
+
+//get the date and time 5 days from now
+DateTime fiveDaysAhead = currentDateTime.AddDays(5);
+Console.WriteLine(fiveDaysAhead);
+
+//get the date and time 5 days and 5 hours from now
+DateTime fiveDaysAndFiveHoursAhead = currentDateTime.AddDays(5).AddHours(5);
+Console.WriteLine(fiveDaysAndFiveHoursAhead);
+
+//get the date and time 2 years and 3 months ago
+DateTime threeMonthsAndTwoYearsAgo = currentDateTime.AddMonths(-3).AddYears(-2);
+Console.WriteLine(threeMonthsAndTwoYearsAgo);
+
+//Formatting datetime to string
+string formattedDate = DateTime.Now.ToString("dd.MM.yyyy"); //11.03.2025
+Console.WriteLine(formattedDate);
+
+string anotherFormattedDate = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+Console.WriteLine(anotherFormattedDate);
