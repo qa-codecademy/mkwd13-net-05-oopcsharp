150eb9b35f0258e9f552dda3c1b66bb869e25e1c
trajanstevkovski <trajanstevkovski@gmail.com>
2025-03-12 18:41:25 +0100

    Class06 classes

--- G3/Class06/QA.SeeSharp.Class06/QA.SeeSharp.Class06.App/Program.cs
+++ G3/Class06/QA.SeeSharp.Class06/QA.SeeSharp.Class06.App/Program.cs
@@ -9,6 +9,23 @@
             user.LastName = "Stevkovski";
             Console.WriteLine(user.FirstName);
 
+            Car car = new Car();
+            car.Type = "Fiat";
+            car.CubicCM = 999;
+            car.User = new User();
+
+            // user.SetAge(12); // method is private and its not usable outside the class implementaiton
+
+            Human human = new Human();
+            human.FirstName = "Trajan";
+            human.LastName = "Stevkovski";
+            human.Age = 34;
+
+            Console.WriteLine(human.GetPersonStats());
+
+            Plane plane = new Plane();
+            Plane plane1 = new Plane("Avion");
+            Plane plane3 = new Plane("Avion1", 100, "Cesna", "Plav", false);
 
             Console.WriteLine("Hello, World!");
         }
