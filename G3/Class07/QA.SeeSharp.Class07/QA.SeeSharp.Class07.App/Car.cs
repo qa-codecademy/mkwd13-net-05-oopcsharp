9f9c68712c8dc5403269367b71b111a3796a31e7
trajanstevkovski <trajanstevkovski@gmail.com>
2025-03-17 19:56:28 +0100

    Class07 inheritance/enums

---
+++ G3/Class07/QA.SeeSharp.Class07/QA.SeeSharp.Class07.App/Car.cs
@@ -0,0 +1,34 @@
+﻿namespace QA.SeeSharp.Class07.App
+{
+    public class Car : WheeledVehicle
+    {
+        public int Doors { get; set; }
+        public string Color { get; set; }
+
+        public Car(string model, string type, int wheels, string color, PetrolType petrol)
+            : base(1, model, type, wheels, petrol)
+        {
+            Console.WriteLine("Constructor of Car");
+            Color = color;
+            Doors = 5;
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine("Im driving a car");
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("In car class");
+            base.Start();
+            Console.WriteLine("brm brm");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stoping car engine");
+            StopEngine();
+        }
+    }
+}
