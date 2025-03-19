c06aa45fe0a4b45cffa7a29e26f9ecbb262a42fd
trajanstevkovski <trajanstevkovski@gmail.com>
2025-03-19 20:19:33 +0100

    Class08 Linq

--- G3/Class08/QA.SeeSharp.Class08/QA.SeeSharp.Class08.App/Program.cs
+++ G3/Class08/QA.SeeSharp.Class08/QA.SeeSharp.Class08.App/Program.cs
@@ -143,8 +143,22 @@ namespace QA.SeeSharp.Class08.App
                 }
             };
 
-            slave.GetFavSongs();
-            trajan.GetFavSongs();
+            //slave.GetFavSongs();
+            //trajan.GetFavSongs();
+
+
+            // LINQ
+
+            List<Dog> dogs = new List<Dog>()
+            {
+                new Dog(){Name = "Sparky", Age = 2},
+                new Dog(){Name = "Butch", Age = 1},
+                new Dog(){Name = "Zoe", Age = 3},
+                new Dog(){Name = "Sia", Age = 1},
+                new Dog(){Name = "William", Age = 4},
+                new Dog(){Name = "Billy", Age = 1},
+                new Dog(){Name = "Buck", Age = 2}
+            };
 
             //Console.ReadLine();
         }
