7fdd556c0bf6dc65070bff47504f80e29fedaf50
Martin <panovski.martin93@gmail.com>
2025-03-20 19:47:03 +0100

    Class 07 demo code added

---
+++ G2/Class07/Qinshift.Class07/Qinshift.Inheritance/Models/Dog.cs
@@ -0,0 +1,27 @@
+﻿namespace Qinshift.Inheritance.Models
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, string type, string color, string breed) 
+            : base(name, type, color)
+        {
+            Console.WriteLine("Dog constructor executed");
+
+            Breed = breed;
+        }
+
+        public Dog(string name) : base(name) { }
+
+        // This Dog class will have every property and method
+        // from class Animal
+
+        // This property will be unique to Dog class
+        public string Breed { get; set; }
+
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} the {Breed} says wooof wooof!!!");
+        }
+
+    }
+}
