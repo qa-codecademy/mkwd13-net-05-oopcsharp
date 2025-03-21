967d5e332148b16aea510c51a4d34640044f0468
trajanstevkovski <trajanstevkovski@gmail.com>
2025-03-21 18:53:49 +0100

    Class09 app

--- G3/Class09/QA.SeeSharp.RentalApp/QA.SeeSharp.RentalApp.App/Program.cs
+++ G3/Class09/QA.SeeSharp.RentalApp/QA.SeeSharp.RentalApp.App/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "Video Rental App";
+
+            
+        }
+
+        static void HomeScreen()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("==========================================================================");
+            Console.WriteLine("==========================================================================");
+            Console.WriteLine("==  \\\\    //  || ||||   ||||||  ||||||   ||||||  ||||||  ||  || ||||||  ==");
+            Console.WriteLine("==   \\\\  //   || ||  || ||=     ||  ||   ||\\\\    ||=     ||\\\\||   ||    ==");
+            Console.WriteLine("==    \\\\//    || ||||   ||||||  ||||||   ||  \\\\  ||||||  ||  ||   ||    ==");
+            Console.WriteLine("==========================================================================");
+            Console.WriteLine("==========================================================================");
+            Console.ResetColor();
+        }
+
+        static void StartMenu()
+        {
+            Console.WriteLine("Welcome to video rent store.");
+            Console.WriteLine("Use the numbers in front of the selection to navigate thru the applicaton.");
+            Console.WriteLine("1. Rent a movie with existing account.");
+            Console.WriteLine("2. Rent a movie and create account.");
+            Console.WriteLine("3. Exit application.");
         }
     }
 }
