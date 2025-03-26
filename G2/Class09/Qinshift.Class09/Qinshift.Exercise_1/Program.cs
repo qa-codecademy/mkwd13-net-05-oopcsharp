57650424ec056af5a881790a30918d7650f98110
Martin <panovski.martin93@gmail.com>
2025-03-26 21:09:36 +0100

    Class 09 part 2 demo code added

--- G2/Class09/Qinshift.Class09/Qinshift.Exercise_1/Program.cs
+++ G2/Class09/Qinshift.Class09/Qinshift.Exercise_1/Program.cs
@@ -29,6 +29,82 @@ List<Movie> MovieSet2 = new List<Movie>() { movie11, movie12, movie13, movie14, 
 Cinema cinema1 = new Cinema("Cineplex");
 cinema1.Halls = new List<int>() { 1, 2, 3, 4 };
 cinema1.Movies = MovieSet1;
+
+
 Cinema cinema2 = new Cinema("Milenium");
 cinema2.Halls = new List<int>() { 1, 2 };
 cinema2.Movies = MovieSet2;
+
+try
+{
+    Console.WriteLine("Choose a cinema:");
+    Console.WriteLine($"1. {cinema1.Name} \n2. {cinema2.Name}");
+
+    int cinemaInput = int.Parse(Console.ReadLine());
+    Cinema currentCinema = new Cinema();
+    if(cinemaInput == 1)
+    {
+        currentCinema = cinema1;
+    }
+    else if(cinemaInput == 2) 
+    {
+        currentCinema = cinema2;
+    }
+    else
+    {
+        throw new Exception("You have to enter 1 or 2! You broke it!");
+    }
+
+    Console.WriteLine("Movies in the cinema:");
+    Console.WriteLine("1.Show all movies \n2.Show by genre");
+    int movieInput = int.Parse(Console.ReadLine());
+    if (movieInput == 1)
+    {
+        Console.WriteLine("Choose a movie to watch: ( Write the name ) ");
+        foreach (var movie in currentCinema.Movies)
+        {
+            Console.WriteLine(movie.Title);
+        }
+        string movieNameInput = Console.ReadLine();
+        Movie movieToWatch = currentCinema.Movies
+                                          .Where(x => x.Title.ToLower().Trim() == movieNameInput.ToLower().Trim())
+                                          .FirstOrDefault();
+        currentCinema.MoviePlaying(movieToWatch);
+    }
+    else if (movieInput == 2)
+    {
+        Console.WriteLine("Enter genre:");
+        Console.WriteLine("1.Comedy");
+        Console.WriteLine("2.Horror");
+        Console.WriteLine("3.Action");
+        Console.WriteLine("4.Drama");
+        Console.WriteLine("5.SCIFI");
+        int genreInput = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Choose movie to watch: ( Write the name ) ");
+        foreach (var movie in currentCinema.Movies.Where(x => x.Genre == (Genre)genreInput))
+        {
+            Console.WriteLine(movie.Title);
+        }
+        string movieByGenreInput = Console.ReadLine();
+
+        Movie movieToWatchByGenre = currentCinema.Movies
+                                          .Where(x => x.Title.ToLower().Trim() == movieByGenreInput.ToLower().Trim())
+                                          .FirstOrDefault();
+
+        currentCinema.MoviePlaying(movieToWatchByGenre);
+    }
+    else
+    {
+        throw new Exception("You have entered a wrong input.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Something went wrong! Try again");
+}
+
+
+
+
+
